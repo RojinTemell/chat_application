ea1cc43d2f0c10d48565bb702b07ec5327a2169e
mertsahiin <mertemre123422@gmail.com>
2023-12-07 20:43:03 +0300

    Maps ve ekran geçişleri ayarlandı

--- lib/features/views/chats_page.dart
+++ lib/features/views/chats_page.dart
@@ -1,15 +1,24 @@
 import 'package:chat_application/features/mixins/navigator_manager.dart';
 import 'package:chat_application/features/views/chat_page.dart';
+import 'package:chat_application/features/views/maps_page.dart';
+import 'package:chat_application/features/views/music_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class ChatsScreen extends StatelessWidget with NavigatorManager {
-   ChatsScreen({super.key});
+class ChatsScreen extends StatefulWidget with NavigatorManager {
+  ChatsScreen({Key? key}) : super(key: key);
 
   final _auth = FirebaseAuth.instance;
 
-  //sor ama başka bir snapshot ı da nasıl kullanabilirim
+  @override
+  _ChatsScreenState createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  int _selectedIndex = 1; // Başlangıç ekranını MusicScreen olarak ayarla
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +31,47 @@ class ChatsScreen extends StatelessWidget with NavigatorManager {
               .copyWith(fontWeight: FontWeight.bold),
         ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
+      body: _getSelectedPage(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        child: Container(
+          color: Colors.black,
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+            child: GNav(
+              backgroundColor: Colors.black,
+              color: Colors.white,
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.grey.shade800,
+              gap: 8,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              selectedIndex: _selectedIndex, // Bu satırı ekleyerek seçili index'i belirtin
+              padding: const EdgeInsets.all(16),
+              tabs: const [
+                GButton(icon: Icons.map, text: 'Maps'),
+                GButton(icon: Icons.music_note, text: 'Music'),
+                GButton(icon: Icons.message, text: 'Message'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getSelectedPage() {
+    switch (_selectedIndex) {
+      case 0:
+        return MapsScreen();
+      case 1:
+        return MusicScreen();
+      case 2:
+        return StreamBuilder<QuerySnapshot>(
           stream: FirebaseFirestore.instance.collection('users').snapshots(),
           builder: (context, snapshot) {
             if (snapshot.hasError) {
@@ -35,31 +84,36 @@ class ChatsScreen extends StatelessWidget with NavigatorManager {
               padding: const EdgeInsets.only(top: 20),
               child: ListView(
                 children: snapshot.data!.docs
-                    .map<Widget>((doc) => _userListItem(doc,context))
+                    .map<Widget>((doc) => _userListItem(doc, context))
                     .toList(),
               ),
             );
-          }),
-    );
+          },
+        );
+      default:
+        return Container();
+    }
   }
 
-  Widget _userListItem(DocumentSnapshot document ,BuildContext context) {
+  Widget _userListItem(DocumentSnapshot document, BuildContext context) {
     Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
 
-    if (_auth.currentUser!.email != data['email']) {
+    if (widget._auth.currentUser!.email != data['email']) {
       return Padding(
         padding: const EdgeInsets.all(10.0),
         child: ListTile(
           trailing: InkWell(
-              onTap: () {
-               navigateToWidget(
-                    context,
-                    ChatScreen(
-                      receiverName: data['email'],
-                      receiverId: data['uid'],
-                    ));
-              },
-              child: const Icon(Icons.chevron_right)),
+            onTap: () {
+              widget.navigateToWidget(
+                context,
+                ChatScreen(
+                  receiverName: data['email'],
+                  receiverId: data['uid'],
+                ),
+              );
+            },
+            child: const Icon(Icons.chevron_right),
+          ),
           leading: const Stack(
             alignment: Alignment.bottomRight,
             children: [
@@ -67,7 +121,7 @@ class ChatsScreen extends StatelessWidget with NavigatorManager {
                 backgroundColor: Colors.white,
                 radius: 30,
                 backgroundImage: NetworkImage(
-                    'https://cdn.bynogame.com/logo/bynocan-1-1678384668185.png'), // burayı sonra kişi profilini doldurunca ekleriz
+                    'https://cdn.bynogame.com/logo/bynocan-1-1678384668185.png'),
               ),
               Padding(
                 padding: EdgeInsets.only(bottom: 10),
@@ -80,12 +134,13 @@ class ChatsScreen extends StatelessWidget with NavigatorManager {
           ),
           title: Text(data['name']),
           onTap: () {
-           navigateToWidget(
-                context,
-                ChatScreen(
-                  receiverName: data['name'],
-                  receiverId: data['uid'],
-                ));
+            widget.navigateToWidget(
+              context,
+              ChatScreen(
+                receiverName: data['name'],
+                receiverId: data['uid'],
+              ),
+            );
           },
         ),
       );
